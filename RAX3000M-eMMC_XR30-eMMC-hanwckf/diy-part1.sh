f9379331bf7e525fa3a61a883e26da78b3ccf2c4
al <66003967+301262177@users.noreply.github.com>
2024-10-26 09:46:25 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -19,5 +19,5 @@
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 # iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
+git https://github.com/linkease/istore-ui app-store-ui
+git https://github.com/linkease/istore luci
